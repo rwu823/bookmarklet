e7ef3ee9040cf9cea4a3c96eae823303aada626e
rwu823 <rwu823@gmail.com>
2021-11-07 14:27:29 +0000

    Update out to output generated at 9b41ca9

---
+++ www.voicetube.com.js
@@ -0,0 +1,6 @@
+(()=>{var o=n=>{let e=document,t=e.createElement("style");t.textContent=n,e.head.append(t)};var c=`.closed-captions { display: none !important; }
+
+.video-subtitles .captions {
+  display: none;
+}
+`;(()=>{o(c),document.querySelector(".video-subtitles")?.addEventListener("click",e=>{let t=e.target,s=t.querySelector(".captions")??t.closest(".captions");if(s){let i=s.offsetParent===null;s.style.display=i?"block":"none"}})})();})();
