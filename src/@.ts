b25deec3c8a3683c8e813bc2d4dff8c44de9d5bb
rocky <rocky@17.media>
2021-11-08 14:29:14 +0800

    fix

--- src/@.ts
+++ src/@.ts
@@ -1,5 +1,12 @@
+/* eslint-disable no-restricted-globals */
+
 import { BOOKMARKLET_CDN } from '@/share/consts'
 import { createScript } from '@/share/createScript'
 ;(() => {
-  createScript(`${BOOKMARKLET_CDN}/main.js`)
+  const { host } = location
+
+  const script = createScript(`${BOOKMARKLET_CDN}/${host}.js`)
+  script.addEventListener('error', () => {
+    alert(`Load script error: \`${host}\``)
+  })
 })()
